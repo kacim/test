08cb13a6c0bd8382c18a5d6608498a3a20d29293
Fionahuang <593253153@qq.com>
2018-11-20 15:02:04 +0800

    login-css

--- web/config.rb
+++ web/config.rb
@@ -1,5 +1,5 @@
 # Require any additional compass plugins here.
-require 'compass-normalize'
+
 #require 'compass/import-once/activate'
 # Set this to the root of your project when deployed:
 http_path = "/resources"
